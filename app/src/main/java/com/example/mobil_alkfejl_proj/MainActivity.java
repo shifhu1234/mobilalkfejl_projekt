6977463f2b3ec5e6aba6991cd9bad630912f0757
Nagy Gábor <h267930@stud.u-szeged.hu>
2025-04-11 15:17:22 +0200

    upload

---
+++ app/src/main/java/com/example/mobil_alkfejl_proj/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mobil_alkfejl_proj;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    EditText userNameET;
+    EditText passwordET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        userNameET = findViewById(R.id.editTextUserName);
+        passwordET = findViewById(R.id.editTextPassword);
+    }
+
+    public void login(View view) {
+//        EditText userName = findViewById(R.id.editTextUserName);
+//        EditText password = findViewById(R.id.editTextPassword);
+
+        String userName = userNameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Log.i(LOG_TAG, "Bejelentkezett: " + userName + ", jelszó: " + password);
+
+    }
+
+    public void register(View view) {
+        //TODO
+
+    }
+}
